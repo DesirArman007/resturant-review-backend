5911fde5503a623dcdba4d1508e69106bc680d8f
Abhishek Yadav <abhishekyadav6502@gmail.com>
2025-07-17 11:42:11 +0530

    Initial project setup

---
+++ src/main/java/com/desirArman/restaurant/domain/entities/User.java
@@ -0,0 +1,14 @@
+package com.desirArman.restaurant.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+}
